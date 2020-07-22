bb1bf9827a278b04ce9e37773ba4c09e0180b399
Marshall.Chiang <ambrosia8286@gmail.com>
2020-07-22 11:06:17 +0800

    item iteration update

--- partner.py
+++ partner.py
@@ -1,3 +1,4 @@
+import collections
 import requests
 import os
 
@@ -10,27 +11,41 @@ class proxy_cursor :
             self.parsed = self.cluster(payload)
 
     def cluster(self, payload) :
-        return self.__formatting__(
+        return [self.__formatting__(
             self.__condition__(
                 self.__constant__(
-                    self.__mapping__(payload)
+                    x
                 )
             )
-        )
+        ) for x in self.__mapping__(payload)]
+        
     def __mapping__(self, p) :
-        result = {}
-        path_tree = [p for p in get_paths(p)]
-        path_tree_last_node = [p[-1] for p in path_tree]
+        resample = collections.defaultdict(list)
         for k, v in self.resource['mapping'].items() :
-            if v in path_tree_last_node :
-                extract_path = path_tree[path_tree_last_node.index(v)]
-                result[k] = recursive_index(p, extract_path)
-        return result
+            resample[v].append(k) 
+        result = collections.defaultdict(list)   
+        max_n = 0
+        for n, p in get_paths(p) :
+            if p[-1] in resample.keys() :
+                for i in resample[p[-1]] :
+                    result[i].append(n)
+                    max_n = len(result[i]) if len(result[i]) > max_n else max_n
+        output = []
+        i = 0
+        while i < max_n :
+            item = {}
+            for k, v in result.items():
+                a = i if i < len(v) else -1
+                item[k] = v[a]
+            output.append(item)
+            i += 1
+        return output
 
     def __constant__(self, p) :
         for k, v in self.resource['constant'].items() : 
             p[k] = v
         return p
+
     def __condition__(self, p) :
         for c in self.resource['condition'] :
             bools = all([p[k] in v for k, v in c['if'].items()])
@@ -40,9 +55,11 @@ class proxy_cursor :
                         v = v[c['if'][k].index(p[k])]
                     p[k] = v
         return p    
+
     def __formatting__(self, p) : 
         for k, v in self.resource['formatting'].items():
             p[k] = eval(v)(p)
+            print(p[k])
         return p
 
 def recursive_index(o, p) :
@@ -60,7 +77,7 @@ def get_paths(d):
     q = [(d, [])]
     while q:
         n, p = q.pop(0)
-        if p : yield p 
+        if p : yield n, p 
         if isinstance(n, dict):
             for k, v in n.items():
                 q.append((v, p+[k]))
